3074177a53498872e4f171147e16175362c654d1
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-03-07 20:28:00 -0300

    feat: added a flux of create room and room entrancing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,8 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
+import { FormEvent, useState } from "react";
+import { database } from "../services/firebase";
 
 
 export function Home() {
@@ -15,6 +17,7 @@ export function Home() {
   let navigate = useNavigate();
 
   const {user, signInWitchGoogle} = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom(){
 
@@ -24,6 +27,23 @@ export function Home() {
 
     navigate("/rooms/new");
 
+  }
+
+  async function handleJoinRoom(event: FormEvent){
+
+    event.preventDefault();
+
+    if(roomCode.trim() === '') {
+      return;
+    }
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()){
+      alert('Room does not exists');
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`);
   }
 
   return (
@@ -43,10 +63,12 @@ export function Home() {
           </button>
           <div className="separator">ou entre em uma sala</div>
 
-          <form action="">
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button 
               type="submit">
