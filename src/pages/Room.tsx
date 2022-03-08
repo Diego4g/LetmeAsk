851cd8af8a7b412c81db927b0b1ba18103c8e715
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-03-07 23:10:34 -0300

    feat: added a functionality for questions listing

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,19 +1,95 @@
+import { FormEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 import "../styles/room.scss";
+
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+}>
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+}
 
 type RoomParams = {
   id: string;
 }
 
 export function Room() {
-
+  const { user } = useAuth();
   const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState('');
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [title, setTitle] = useState('');
 
-  const roomId = params.id as string
+  const roomId = params.id as string;
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    //once é o evento de ouvir uma vez, on é o evento de ficar ouvindo, no caso abaixo estamos ouvindo o 'value'
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions:FirebaseQuestions  = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighlighted: value.isHighlighted,
+          isAnswered: value.isAnswered,
+        }
+      });
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    })
+  }, []);
+
+  async function hanldeSendQuestion(event: FormEvent){
+
+    event.preventDefault();
+
+    if(newQuestion.trim() === ''){
+      return;
+    }
+
+    if(!user){
+      throw new Error('You must be logged in');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
+  }
 
   return (
     <div id="page-room">
@@ -26,19 +102,30 @@ export function Room() {
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
         </div>
 
-        <form action="">
+        <form onSubmit={hanldeSendQuestion}>
         <textarea 
           placeholder="O que você quer perguntar?"
+          onChange={event => setNewQuestion(event.target.value)}
+          value={newQuestion}
         /> 
         <div className="form-footer">
-          <span>Para enviar uma pergunta, <button> faça seu login. </button> </span> 
-          <Button type="submit">Enviar pergunta</Button>         
+          { user ? (
+            <div className="user-info">
+              <img src={user.avatar} alt={user.name} /> 
+              <span>{user.name}</span> 
+            </div>
+          ) : (
+            <span>Para enviar uma pergunta, <button> faça seu login. </button> </span> 
+          )}
+          <Button type="submit" disabled={!user}>Enviar pergunta</Button>         
         </div>
         </form>
+
+        {JSON.stringify(questions)}
       </main>
     </div>
 
