14d4d84cd76d895b4376f6e2112bbd506bf8477c
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-03-08 20:24:46 -0300

    feat: added a remove room functionality

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import logoImg from "../assets/images/logo.svg";
 import deleteImg from "../assets/images/delete.svg";
@@ -20,11 +20,21 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
+
+  let navigate = useNavigate();
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const roomId = params.id as string;
 
   const { title, questions } = useRoom(roomId);
+
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date(),
+    })
+
+    navigate('/');
+  }
 
   async function handleDeleteQuestion(questionId: string){
     if(window.confirm("Tem certeza que deseja excluir esta pergunta?")){
@@ -39,7 +49,7 @@ export function AdminRoom() {
           <img src={logoImg} alt="Letmeask" />
           <div>
             <RoomCode code={roomId}/>
-            <Button isOutlined>Encerrar sala</Button>
+            <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
           </div>
         </div>
       </header>
